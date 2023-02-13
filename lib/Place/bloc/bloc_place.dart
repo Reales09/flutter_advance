78ccee413e36b645ac2143be9f4c31a4f15fd539
AZ-desarrollador4 <desarrollador4@azsmart.net>
2023-02-13 17:20:11 -0500

    Se agrega bloc y se conecta con firebase

---
+++ lib/Place/bloc/bloc_place.dart
@@ -0,0 +1,9 @@
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+class PlaceBloc implements  Bloc{
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:prueba/User/bloc/bloc_user.dart';
 import 'package:prueba/firebase_options.dart';
 import 'platzi_trips.dart';
 import 'platzi_trips_cupertino.dart';
@@ -21,23 +23,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider(bloc: UserBloc(), child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-
       ),
       //home: PlatziTrips()
       home: PlatziTripsCupertino(),
-    );
+    ));
   }
 }
 
