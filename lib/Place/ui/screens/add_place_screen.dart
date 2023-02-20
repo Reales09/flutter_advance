8b384b2dc423ac1de9c5cde9207fccafa01ff69a
AZ-desarrollador4 <desarrollador4@azsmart.net>
2023-02-19 23:41:55 -0500

    Se agrega input location y text input en la addScreen

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:prueba/Place/ui/widgets/title_input_location.dart';
 import 'package:prueba/Widgets/gradient_back.dart';
+import 'package:prueba/Widgets/text_input.dart';
 import 'package:prueba/Widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -26,12 +28,15 @@ class _AddPlaceScreen extends State<AddPlaceScreen>{
     // TODO: implement build
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+
 
     return Scaffold(
       body: Stack(
         children: [
           GradientBack(height: 300.0),
-          Row(
+          Row(//AppBar
             children: [
               Container(
                 padding: EdgeInsets.only(top: 25.0, left: 5.0),
@@ -53,8 +58,32 @@ class _AddPlaceScreen extends State<AddPlaceScreen>{
                   child: TitleHeader(title: "Add a new Place"),
                 ),
               ))
-
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: [
+                Container(//foto
+
+                ),
+                Container(//TextFiel title
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(hintText: "Title", inputType: TextInputType.text,maxLines: 1, controller: _controllerTitlePlace),
+                ),
+                TextInput
+                  (hintText: "Description",
+                    inputType: TextInputType.multiline,
+                    maxLines: 4,
+                    controller: _controllerDescriptionPlace
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: TextInputLocation(hintText: "Add Location",
+                      iconData: Icons.location_on_outlined),
+                )
+              ],
+            ),
           )
         ],
       ),
---
+++ lib/Place/ui/widgets/title_input_location.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class TextInputLocation extends StatelessWidget{
+  final String hintText;
+  final IconData iconData;
+
+
+  TextInputLocation({
+    required this.hintText,
+    required this.iconData
+});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Container(
+     padding: EdgeInsets.only(right: 20.0, left: 20.0),
+     child: TextField(
+     style: TextStyle(
+       fontSize: 15.0,
+       fontFamily: "Lato",
+       color: Colors.blueGrey,
+       fontWeight: FontWeight.bold
+       ),
+       decoration: InputDecoration(
+         hintText: hintText,
+         suffixIcon: Icon(iconData),
+         fillColor: Color(0xFFFFFFFF),
+         filled: true,
+         border: InputBorder.none,
+         enabledBorder: OutlineInputBorder(
+             borderSide: BorderSide(color: Color(0xFFFFFFFF)),
+             borderRadius: BorderRadius.all(Radius.circular(12.0))
+         ),
+         focusedBorder: OutlineInputBorder(
+             borderSide: BorderSide(color: Color(0xFFFFFFFF)),
+             borderRadius: BorderRadius.all(Radius.circular(12.0))
+         ),
+
+       ),
+      ),
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 15.0,
+            offset: Offset(0.0,7.0)
+          )
+        ]
+      ),
+    );
+
+  }
+}
